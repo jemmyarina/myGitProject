500558e3f15320b176c034d7c8165fc49328fa28
jemmyarina <jemmyarinau@gmail.com>
2020-11-11 14:19:30 +0200

    blog commenting by logged in user but not finished

---
+++ scripts/signIn_comment.js
@@ -0,0 +1,45 @@
+const signInForm=document.querySelector(".form_container");
+signInForm.addEventListener('submit', (e)=>{
+e.preventDefault();
+const email=document.getElementById("email").value;
+const password=document.getElementById("password").value;
+const message=document.getElementById("message");
+if(email=='')
+    {   message.style.display="block";
+        message.innerHTML='Please, enter email';
+        return false;
+    }
+    else if(password==''){
+        message.style.display="block";
+        message.innerHTML='Please, enter password';
+        return false;
+    }
+    else{
+    auth.signInWithEmailAndPassword(email, password).then(resultData=>{
+
+    const uid= resultData.user.uid; 
+    signInForm.reset();
+
+    db.collection("Users").doc(uid).get().then( function(snapshot){
+  
+        var childData = snapshot.data();
+         
+        var lastname = childData.lname;
+        var lnameUpper = lastname.substring(0, 1);
+        var firstname = childData.fname;
+        var emailFetched = childData.email;
+
+        sessionStorage.setItem("userUID", uid);
+        sessionStorage.setItem("uNames", lnameUpper+ '.' +firstname);
+        sessionStorage.setItem("userEmail", emailFetched);
+    
+        window.location.href = "../pages/blog_read_more.html";
+       
+        });
+})
+.catch(error=>{
+    
+    alert(error.message);
+});
+}
+})
